e32acecb8517a504af20695f5e92ab3067e66dcb
mselmi <mselmi@mariems-MacBook-Pro.local>
2020-11-21 14:10:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+  <>
+  <div style={{border:"solid 1px black", maxWidth:"100vw"}}>
+  <div className='titlered'>Name</div>
+  <br/>
+  <img src="./bordgena.jpeg"/>
+  <br/>
+  <img src="./lemon.jpg"/>
+  </div>
+  <video width="320" height="240" controls/>
+  </>
+
+  );
+}
+
+export default App;
